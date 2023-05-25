4517d7dd4c998d752bd79c631f66ea5b8946eb64
brvnfr <ymmk93@gmail.com>
2023-05-25 17:19:54 -0300

    Initial commit

---
+++ app/src/main/java/com/vendorhub/twa/MainActivity.java
@@ -0,0 +1,26 @@
+package com.vendorhub.twa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("https://develop.d35ur8hsgk7q93.amplifyapp.com");
+    }
+}
